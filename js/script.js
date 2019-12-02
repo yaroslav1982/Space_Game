3091e72ed08f0c9b5529f77654f8d569ae9b4ac9
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-02 16:07:02 +0300

    anim

--- js/script.js
+++ js/script.js
@@ -46,7 +46,8 @@ const SPIN = new function () {
             nodes[i].update();
             nodes[i].draw();
         }
-    }
+        requestAnimationFrame(SPIN.update);
+    };
 
 
     SPIN.start = (W, H) => {
