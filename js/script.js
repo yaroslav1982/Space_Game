4392b0d4092feaff4b9a98451b1ca4e0d53572ad
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-03 16:58:05 +0300

    патроны

--- js/script.js
+++ js/script.js
@@ -141,7 +141,7 @@ window.addEventListener('load', function () {
     }
 
     let fire = (x, y) => {
-        if (SPIN.get_timer() > 10) {
+        if (SPIN.get_timer() > 15) {
             SPIN.create_node(x, y, 10, 20, '#14ff00', bullet_ai);
             SPIN.clear_timer();
         }
